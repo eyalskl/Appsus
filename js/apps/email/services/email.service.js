e171bfe0701bee20c85e13748db3ab7d3010d49c
Eyal Barkai <eyalskl18@gmail.com>
2020-06-24 13:37:31 +0300

    2nd commit

---
+++ js/apps/email/services/email.service.js
@@ -0,0 +1,29 @@
+import { utilsService } from '../../../services/utils.service.js'
+
+const EMAILS_KEY = 'emails'
+
+export const emailService = {
+    createDefaultEmails,
+    getDefaultEmails
+}
+
+var gEmails = getDefaultEmails()
+
+function getDefaultEmails() {
+    return Promise.resolve(gEmails)
+}
+
+function createDefaultEmails() {
+    const emails = utilsService.loadFromStorage(EMAILS_KEY);
+    if (emails) return emails;
+    const defaultEmails = [
+        {from: 'eyal' ,subject: 'Wassap?', body: 'Pick up!', isRead: false, sentAt : 1592994373656},
+        {from: 'eyal' ,subject: 'EIZE ABALE', body: 'just some text to test', isRead: false, sentAt : 1592994373656},
+        {from: 'eyal' ,subject: 'Sprint 3 SUSIM', body: 'just some text to test', isRead: false, sentAt : 1592994373656},
+        {from: 'eyal' ,subject: 'Hi there!', body: 'just some text to test', isRead: false, sentAt : 1592994373656},
+        {from: 'eyal' ,subject: 'You are FIRED!', body: 'just some text to test', isRead: false, sentAt : 1592994373656},
+
+    ];
+    utilsService.storeToStorage(EMAILS_KEY, defaultEmails)
+    return defaultEmails
+}
