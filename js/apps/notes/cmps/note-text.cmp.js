7d231b14b092fec76b4dff78b6706af0e30bf69b
Eyal Barkai <eyalskl18@gmail.com>
2020-06-27 17:42:57 +0300

    123

--- js/apps/notes/cmps/note-text.cmp.js
+++ js/apps/notes/cmps/note-text.cmp.js
@@ -1,16 +1,22 @@
 export default {
     name: 'noteText',
+    props: ['info', 'edit'],
     template: `
-          <div class="note-desc text-container">
-            <h3>{{info.txt}}</h3>
-            <i class="fas fa-font note-type"></i>
-</div>
+        <div class="note-desc text-container">
+            <div v-if="!edit">
+                <h3>{{info.txt}}</h3>
+                <i class="fas fa-font note-type"></i>
+            </div>
+            <div v-else class="note-edit">
+                <input v-model="txt" type="text" />
+                <button> Confirm </button>
+            </div>
+        </div>
           `,
-    props: ["info"],
     data() {
         return {
-            val: ""
-        };
+            txt: this.info.txt || '',
+        }
     },
     methods: {
 
