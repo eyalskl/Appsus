78810b06dbd8ac9c7c057e23e7b3d87b3af24fee
Eyal Barkai <eyalskl18@gmail.com>
2020-07-06 20:27:50 +0300

    fixed dixed

--- js/apps/email/cmps/email-list.cmp.js
+++ js/apps/email/cmps/email-list.cmp.js
@@ -4,34 +4,11 @@ export default {
     name: 'email-list',
     props: ['emails'],
     template: `
-        <div>
             <ul class="email-list clean-list flex column align-center">
-                <draggable v-bind="dragOptions" class="drag-zone" tag="div" :email="email"  @start="drag=true" @end="drag=false">
-                    <transition-group class="transition-container flex wrap" type="transition"  :name="!drag ? 'flip-list' : null">
-                        <email-preview v-for="email in emails" :email="email" :key="email.id"/>
-                    </transition-group>
-                </draggable>
+                <email-preview v-for="email in emails" :email="email" :key="email.id"/>
             </ul>
-        </div>
     `,
-    data() {
-        return {
-            email: this.email,
-            drag: false,
-        }
-    },
     components: {
         emailPreview,
-    },
-    computed: {
-        dragOptions() {
-            return {
-                animation: 300,
-                group: "description",
-                disabled: false,
-                ghostClass: "ghost"
-            }
-        },
-
     }
 };
